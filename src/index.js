401f88bc72297ac93e8b28c5d2e35dec64ef72ed
Mike Duval <mikeduval42@gmail.com>
2023-03-02 12:35:12 -0800

    Removals to eliminate error
    -delete package-lock.json
    -delete node-modules folder

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { asyncWithLDProvider } from "launchdarkly-react-client-sdk";
 import { deviceType, osName } from "react-device-detect";
 import getUserId from "./util/getUserId";
 
-const CLIENTKEY = "609ead905193530d7c28647b";
+const CLIENTKEY = "63616ff3dcebd8115518f618";
 
 let id = getUserId();
 
